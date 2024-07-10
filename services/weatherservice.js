e6688e6008a219b33e6533d0a9477ef1c8a90524
elif rana sakin <bayarberhan@gmail.com>
2024-07-10 14:57:16 +0300

    api update

--- services/weatherservice.js
+++ services/weatherservice.js
@@ -1,3 +1,5 @@
+import {DateTime} from "luxon";
+
 const API_KEY ='ebbde961e4a343eca1f131943240907';
 const BASE_URL = 'http://api.weatherapi.com/v1/';
 
@@ -9,4 +11,36 @@ const getWeatherData = (infoType, searchParams) => {
     .then((data) => data);
 }
 
-export default getWeatherData
+
+
+const formatToLocalTime = (
+    secs,
+    offset,
+    format = "cccc, dd LLL yyyy' | Yerel zaman: 'hh:mm a"
+) => DateTime.fromSeconds(secs + offset, {zone:"utc"}).toFormat(format);
+
+const formatCurrent = (data) => {
+    const { coord: {lat, lon}, 
+            main: {temp, feelsLike, tempMin, tempMax, humidity},
+            name, dt, 
+            sys: {country, sunrise, sunset}, weather, 
+            wind:{speed},
+            timezone,} = data;
+
+            const {main: details, icon} = weather[0];
+            const formattedLocalTime = formatToLocalTime(dt, timezone);
+    return {
+        temp, feelsLike, tempMin, tempMax, humidity, name, country, sunrise: formatToLocalTime(sunrise, timezone, 'hh:mm a'),
+        sunset: formatToLocalTime(sunset, timezone, 'hh:mm a'), speed, details, icon
+    }
+}
+
+const getFormattedWeatherData = async (searchParams) => {
+    const formattedCurrentWeather = await 
+    getWeatherData("current.json", searchParams).then(formatCurrent);
+
+    return {...formattedCurrentWeather};
+
+}
+
+export default getFormattedWeatherData
